7b4e9d9823b9bd0ad0726b5ee0f0b38acac6f4eb
haruki <haruki1015go@gmail.com>
2025-03-31 23:08:56 +0900

    [add]タスクの追加と削除を追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,70 @@
 import "./App.css";
 import db from ".";
-import { addDoc, collection, getDocs } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  getDocs,
+  query,
+  orderBy,
+  doc,
+  updateDoc
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
+import DeleteTaskButton from "./components/deleteTaskButton";
 
 function App() {
   const [title, setTitle] = useState("");
+  const [tasks, setTasks] = useState([]); // タスクリストの状態を管理
+
+  // データベースのフィールドを完了にする
+  const handleConpleteTask = async (id, prevCopleted) => {
+    console.log("タスクのID:", id)
+    /**
+     * チェックしたタスクのidを取得
+     * 一致したタスクのinputをcheckedに
+     * それとtasksのフィールドを更新
+     **/
+    const taskRef = doc(db, "task", id);
+    await updateDoc(taskRef, {
+      conpleted: !prevCopleted
+    });
+    fetchData();
+  //   const checked = e.target.checked;
+  //   if (checked) {
+  //     executeFunction(); // チェック時のみ関数を実行
+  //   }
+  // };
+  // // 実行する関数
+  // const executeFunction = async (id) => {
+  //   console.log("Checkbox is checked!", id);
+  //   他の処理をここに追加
+  //   const washingtonRef = doc(db, "task", id);
+  //   await updateDoc(washingtonRef, {
+  //     conpleted: true
+  //   });
+  };
+
+  const fetchData = async () => {
+    try {
+      const taskQuery = query(
+        collection(db, "task"),
+        orderBy("createdAt", "asc")
+      );
+      const querySnapshot = await getDocs(taskQuery);
+      const taskList = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setTasks(taskList); // 状態にセット
+    } catch (error) {
+      console.log("Error fetching tasks: ", error);
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      const querySnapshot = await getDocs(collection(db, "posts"));
-      if (querySnapshot.empty) {
-        console.log("No documents found in the 'posts' collection.");
-      } else {
-        querySnapshot.forEach((doc) => {
-          console.log(doc.id, " => ", doc.data());
-        });
-      }
-    };
     fetchData();
   }, []);
+
 
   // 追加をクリックするとfirestoreにタスクが追加される
   const handleAddTask = async () => {
@@ -27,21 +73,47 @@ function App() {
       //docRefはDocument Reference（ドキュメント参照）
       const docRef = await addDoc(collection(db, "task"), {
         title: title,
+        createdAt: new Date(), // 現在時刻を追加
       });
       console.log("Document written with ID: ", docRef.id, title);
       setTitle("");
+      fetchData();
     } catch (error) {
       console.error("Error adding document: ", error);
     }
   };
+
+  // エンターキーが押されたときにタスクを追加する
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" && title !== "") {
+      handleAddTask();
+    }
+  };
+
   return (
     <div className="App">
-      <input
-        type="text"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <button onClick={() => handleAddTask()} disabled={title === ""}>追加</button>
+      <div className="inner">
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+        <button onClick={() => handleAddTask()} disabled={title === ""}>
+          追加
+        </button>
+        <div className="taskList">
+          <ul>
+            {tasks.map((task) => (
+              <li className="taskList" key={task.id}>
+                <input type="checkbox" checked={task.conpleted} onChange={() => handleConpleteTask(task.id,task.conpleted)} />
+                {task.title}
+                <DeleteTaskButton db={db} fetchData={fetchData} id={task.id} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/deleteTaskButton.jsx
@@ -0,0 +1,19 @@
+import { deleteDoc, doc } from "firebase/firestore";
+
+//データベースから削除
+const DeleteTaskButton = (props) => {
+  const {db, fetchData, id } = props;
+  console.log(props)
+  const handleDeleteTask = async (id) => {
+    try {
+      await deleteDoc(doc(db, "task", id));
+      console.log("Document written with ID: ", id);
+      fetchData();
+    } catch (error) {
+      console.error("deleteできませんでした", error);
+    }
+  };
+  return <button onClick={() => handleDeleteTask(id)}>x</button>;
+};
+
+export default DeleteTaskButton;
